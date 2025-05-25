e0c35bf46e1fddc6f5b1312a4c49695fc6c5fbf7
fsegovia <felipe.segovia.rod@gmail.com>
2025-05-24 21:17:41 -0400

    Configuracion de Shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button';
+
 export const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-500">
+    <div className="flex flex-col items-center justify-center min-h-svh bg-gray-500">
       <h1 className="text-2xl">Reportaje Ciudadano</h1>
+      <Button>Click me</Button>
     </div>
   );
 };
